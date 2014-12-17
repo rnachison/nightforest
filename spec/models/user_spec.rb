a53746ac2f500e62b0fea74bab5338676a7c8206
rnachison <rnachison@gmail.com>
2014-12-17 11:24:40 -0800

    begin to reconfigure tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,28 @@ require "rails_helper"
 
 describe User do
 
-    it { should validate_uniqueness_of(:email) }
+    # it { should validate_uniqueness_of(:email) }
 
-    it { should validate_uniqueness_of(:username) }
+    # it { should validate_uniqueness_of(:username) }
 
-    it do
-      should allow_value('a@a.com').
-                 for(:email)
-    end
+    # it do
+    #   should allow_value('a@a.com').
+    #              for(:email)
+    # end
 
-    it do
-      should !allow_value('bob').
-                 for(:email)
-    end
+    # it do
+    #   should !allow_value('bob').
+    #              for(:email)
+    # end
 
-    it { should ensure_length_of(:password).is_at_least(6) }
+    # it { should ensure_length_of(:password).is_at_least(6) }
 
-    it { should ensure_length_of(:password).is_at_most(20)}
+    # it { should ensure_length_of(:password).is_at_most(20)}
 
-    it { should ensure_length_of(:email).is_at_most(255)}
+    # it { should ensure_length_of(:email).is_at_most(255)}
 
-    it { should ensure_length_of(:username).is_at_least(1) }
+    # it { should ensure_length_of(:username).is_at_least(1) }
 
-    it { should ensure_length_of(:username).is_at_most(15)}
+    # it { should ensure_length_of(:username).is_at_most(15)}
 
 end
