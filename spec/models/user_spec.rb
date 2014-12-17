2607cc5aa53bbe2c3ba2af7a4ad02e9ef81cffec
rnachison <rnachison@gmail.com>
2014-12-17 10:59:04 -0800

    add validations to user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 
 describe User do
 
-    it "has an email" do
-      user = FactoryGirl.build_stubbed(:a_user)
-      expect(user.email).to_not be_nil  
+    it { should validate_uniqueness_of(:email) }
+
+    it do
+      should allow_value('a@a.com').
+                 for(:email)
     end
 
-    it "has a password" do
-      user = FactoryGirl.build_stubbed(:a_user)
-      expect(user.password).to_not be_nil
+    it do
+      should !allow_value('bob').
+                 for(:email)
     end
 
+    it { should ensure_length_of(:password).is_at_least(6) }
+
+    it { should ensure_length_of(:password).is_at_most(20)}
+
+    it { should ensure_length_of(:email).is_at_most(255)}
+    
 end
