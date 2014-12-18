833659aa72a1eefbf810e87e57cba2ed4aef57e0
rnachison <rnachison@gmail.com>
2014-12-18 11:33:19 -0800

    change exoplanet colors

--- app/controllers/night_sky_controller.rb
+++ app/controllers/night_sky_controller.rb
@@ -5,7 +5,7 @@ class NightSkyController < ApplicationController
 	end
 
 	def exoplanets
-		uri3 = HTTParty.get'http://www.asterank.com/api/kepler?query=%7B%7D&limit=1000'
+		uri3 = HTTParty.get'http://www.asterank.com/api/kepler?query=%7B%22TPLANET%22:%7B%22$gt%22:0%7D%7D&limit=1000'
 		@exo = JSON.parse(uri3.body)
 	end
 
